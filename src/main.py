89f01cf376f261736d7e278055e7f1dc4bf04b5f
J.H <59863142+JoenHo@users.noreply.github.com>
2022-05-25 13:43:44 -0700

    Created initial main.py and items.json

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def read_input_file():
+    print("modify function here")
+
+
+def select_items():
+    print("modify funtion here")
+
+
+def main():
+    # Read Input File
+    read_input_file()
+
+    # Ask User to Select Items
+    select_items()
+
+    # Start Bagging
+
+    # Display Result
+
+
+if __name__ == '__main__':
+    main()
