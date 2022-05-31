2fa4da9c86af07b510a246721f8cab7840f3815f
Luoshan Zhang <lzhang@seattleu.edu>
2022-05-31 16:11:19 -0700

    Updated start bagging function

--- src/main.py
+++ src/main.py
@@ -166,8 +166,83 @@ def display_sub_menu(bg:Bagger):
         except ValueError:
             console.print("Invalid Input", style="red")
 
-def display_start_bagging(bg:Bagger):
+
+def sorting(item_list):
+    # Return sorted item list
+    # Sort: point =  0.5 * weight + 0.5 * volume + rigidity
     print()
+
+
+def meat_seafood_bagging(meat_seafood_list, bg:Bagger):
+    total_weight = 0
+    total_volume = 0
+
+    print('\nStart bagging meat and seafood items')
+    for item in meat_seafood_list:
+        total_volume += (item[0].volume * item[1])
+        total_weight += (item[0].weight * item[1])
+        print(f"name: {item[0].name}")
+        print(f"quantity: {item[1]}")
+
+    volume_bag = int(total_volume / bg.BAG_VOLUME_CAPACITY) + (total_volume %
+                                                    bg.BAG_VOLUME_CAPACITY > 0)
+    weight_bag = int(total_weight / bg.BAG_WEIGHT_CAPACITY) + (total_weight %
+                                                    bg.BAG_WEIGHT_CAPACITY > 0)
+    estimate_bag = max(volume_bag, weight_bag)
+    print(f"Estimated bags: {estimate_bag}")
+
+
+def frozen_bagging(frozen_list, bg:Bagger):
+    print('\nStart bagging frozen items')
+    for item in frozen_list:
+        print(f"name: {item[0].name}")
+        print(f"quantity: {item[1]}")
+
+
+def food_bagging(food_list, bg:Bagger):
+    print('\nStart bagging food items')
+    for item in food_list:
+        print(f"name: {item[0].name}")
+        print(f"quantity: {item[1]}")
+
+
+def non_food_bagging(non_food_list, bg:Bagger):
+    print('\nStart bagging non-food items')
+    for item in non_food_list:
+        print(f"name: {item[0].name}")
+        print(f"quantity: {item[1]}")
+
+
+def display_start_bagging(bg:Bagger):
+    item: Bagger.selected
+    items = bg.get_selected()
+
+    meat_seafood_list = []
+    frozen_list = []
+    food_list = []
+    non_food_list = []
+
+    for item in items:
+        if item[0].category == 'meat' or item[0].category == 'seafood':
+            meat_seafood_list.append(item)
+        elif item[0].category == 'frozen':
+            frozen_list.append(item)
+        elif item[0].category == 'vegetable' or item[0].category == 'fruit' \
+                or item[0].category == 'dairy' or item[0].category == \
+                'beverage' or item[0].category == 'snack' or item[0].category\
+                == 'other_food':
+            food_list.append(item)
+        else:
+            non_food_list.append(item)
+
+    if len(meat_seafood_list):
+        meat_seafood_bagging(meat_seafood_list, bg)
+    if len(frozen_list):
+        frozen_bagging(frozen_list, bg)
+    if len(food_list):
+        food_bagging(food_list, bg)
+    if len(non_food_list):
+        non_food_bagging(non_food_list, bg)
 
 
 def select_items(bg:Bagger, item_list):
@@ -206,6 +281,25 @@ def main():
 
     # Display Main Menu
     display_main_menu(bg)
+
+    # # Mock that user selected item
+    # item1 = 1
+    # item2 = 3
+    # item3 = 23
+    # item4 = 20
+    # item5 = 7
+    # selected = next((x for x in items if x.id == item1), None)
+    # bg.add_to_selected(selected, 5)
+    # selected = next((x for x in items if x.id == item2), None)
+    # bg.add_to_selected(selected, 1)
+    # selected = next((x for x in items if x.id == item3), None)
+    # bg.add_to_selected(selected, 2)
+    # selected = next((x for x in items if x.id == item4), None)
+    # bg.add_to_selected(selected, 1)
+    # selected = next((x for x in items if x.id == item5), None)
+    # bg.add_to_selected(selected, 2)
+
+    display_start_bagging(bg)
 
     # Ask User to Select Items
 
