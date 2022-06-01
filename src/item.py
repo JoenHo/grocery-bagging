9ae0737ddd52dae2e68424afe1db259ef0e49b63
J.H <59863142+JoenHo@users.noreply.github.com>
2022-05-31 17:04:04 -0700

    Add hashing and quality to Item class

--- src/item.py
+++ src/item.py
@@ -11,6 +11,17 @@ class Item():
         self.rigidity = rigidity
         self.image = image
 
+    
+    def __eq__(self, __o: object) -> bool:
+        # use id for equality check
+        return isinstance(object, Item) and self.id == object.id
+
+
+    def __hash__(self) -> int:
+        # use id for hashcode
+        return hash(self.id)
+
+
 class ItemDecoder(json.JSONDecoder):
     def __init__(self, object_hook=None, *args, **kwargs):
         super().__init__(object_hook=self.object_hook, *args, **kwargs)
